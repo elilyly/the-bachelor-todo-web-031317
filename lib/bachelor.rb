ab4a44aa07c765c2904f11c395a64b17ecf4ead5
Elizabeth Nicholson <elizabethnich19@gmail.com>
2017-03-12 17:06:10 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,24 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].detect  { |contestant| contestant["status"] == "Winner"}["name"].split(" ").first
 end
 
+
 def get_contestant_name(data, occupation)
-  # code here
+  all_contestants = data.values.reduce([]) { |all_contestants, contestants| all_contestants.concat(contestants) }
+  all_contestants.detect  { |contestant| contestant["occupation"] == occupation }["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  all_contestants = data.values.reduce([]) { |all_contestants, contestants| all_contestants.concat(contestants) }
+  all_contestants.select { |contestant| contestant["hometown"] == hometown }.length
 end
 
 def get_occupation(data, hometown)
-  # code here
+  all_contestants = data.values.reduce([]) { |all_contestants, contestants| all_contestants.concat(contestants) }
+  all_contestants.detect  { |contestant| contestant["hometown"] == hometown }["occupation"]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = data[season].collect {|contestant| contestant["age"].to_i}
+  (ages.reduce(0) { |sum, age| sum + age }.to_f / ages.length).round
 end
